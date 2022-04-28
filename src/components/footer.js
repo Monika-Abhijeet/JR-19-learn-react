d1a009ce01fdb63d579d01719ef3e87ecc7bf933
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-04-28 17:10:35 +0530

    event handling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,30 @@
 import { Component } from "react";
 
 class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "Monika",
+      lastName: "Abhijeet",
+    };
+  }
+  sayHello = () => {
+    console.log("HIIIII", this.state.firstName);
+  };
+  changeFirstName = (name) => {
+    this.setState({ firstName: name });
+  };
   render() {
-    return <div>{this.props.email}</div>;
+    return (
+      <div>
+        <p>{this.props.email}</p>
+        <p>First name is {this.state.firstName}</p>
+        <button onClick={() => this.sayHello()}>say Hi</button>
+        <button onClick={() => this.changeFirstName("Gowri")}>
+          Change Name
+        </button>
+      </div>
+    );
   }
 }
 export default Footer;
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,8 +1,12 @@
 function Hello(props) {
+  let sayHello = (message) => {
+    console.log(`hello ${message}`);
+  };
   return (
     <div>
       <h4>Hello {props.name}, welcome to react class</h4>
       <p>{props.description} </p>
+      <button onClick={() => sayHello("Monika")}>Click Me</button>
     </div>
   );
 }
