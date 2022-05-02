5fd5a01f85dcb44b8f617ea71ea4675349cdbf45
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-05-02 12:31:38 +0530

    schema validation

--- src/pages/forms/signup.js
+++ src/pages/forms/signup.js
@@ -3,15 +3,19 @@ import * as Yup from "yup";
 
 function Signup() {
   const signupSchema = Yup.object().shape({
+    firstname: Yup.string("First name should be a string").required(
+      "firstname is required"
+    ),
+    age: Yup.number("age should be a number").required(),
     email: Yup.string().email("invalid email").required("email cant be empty"),
     password: Yup.string()
       .required("Password cant be empty")
-      .test("len", "very week", (val) => val.length > 5)
-      .test("len", "week", (val) => val.length > 8),
-    confirmPassword: Yup.string()
-      .required("Password cant be empty")
-      .test("len", "very week", (val) => val.length > 5)
-      .test("len", "week", (val) => val.length > 8),
+      .min(8, "password week")
+      .max(16, "password too long"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "password should match"
+    ),
   });
 
   let handleSubmit = (values) => {
@@ -21,12 +25,32 @@ function Signup() {
     <div>
       <h1>Sign up</h1>
       <Formik
-        initialValues={{ email: "", password: "", confirmPassword: "" }}
-        onSubmit={(e) => handleSubmit()}
+        initialValues={{
+          firstname: "",
+          age: 0,
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={handleSubmit}
         validationSchema={signupSchema}
       >
         {(props) => (
           <Form>
+            <div>
+              <label>First Name</label>
+              <Field type="text" name="firstname" />
+              <ErrorMessage name="firstname">
+                {(error) => <p>{error}</p>}
+              </ErrorMessage>
+            </div>
+            <div>
+              <label>Age</label>
+              <Field type="text" name="age" />
+              <ErrorMessage name="age">
+                {(error) => <p>{error}</p>}
+              </ErrorMessage>
+            </div>
             <div>
               <label>Email</label>
               <Field type="email" name="email" />
@@ -36,14 +60,14 @@ function Signup() {
             </div>
             <div>
               <label>Password</label>
-              <Field type="password" name="password" />
+              <Field type="text" name="password" />
               <ErrorMessage name="password">
                 {(error) => <p>{error}</p>}
               </ErrorMessage>
             </div>
             <div>
               <label>Confirm Password</label>
-              <Field type="password" name="confirmPassword" />
+              <Field type="text" name="confirmPassword" />
               <ErrorMessage name="confirmPassword">
                 {(error) => <p>{error}</p>}
               </ErrorMessage>
