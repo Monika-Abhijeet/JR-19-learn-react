5b4cef0f9ab66f0e6cd92868752f984245eb83ea
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-05-02 17:26:00 +0530

    accesing backend isnide react

--- src/components/httpRequests/axiosRequests.js
+++ src/components/httpRequests/axiosRequests.js
@@ -1,10 +1,17 @@
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function AxiosRequests() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   let getPosts = () => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => console.log(res.data));
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setPosts(res.data);
+    });
   };
 
   let post = () => {
@@ -18,28 +25,37 @@ function AxiosRequests() {
       .then((res) => console.log(res.data));
   };
 
-  let getUser = () => {
-    axios
-      .get("http://localhost:8080/employee")
-      .then((res) => console.log(res.data));
-  };
-  let postUser = () => {
-    let requestBody = {
-      name: "Monika",
-      designation: "CEO",
-      salary: 90000,
-    };
-    axios
-      .post("http://localhost:8080/addemployee", requestBody)
-      .then((res) => console.log(res.data));
-  };
+  //   let getUser = () => {
+  //     axios
+  //       .get("http://localhost:8080/employee")
+  //       .then((res) => console.log(res.data));
+  //   };
+  //   let postUser = () => {
+  //     let requestBody = {
+  //       name: "Monika",
+  //       designation: "CEO",
+  //       salary: 90000,
+  //     };
+  //     axios
+  //       .post("http://localhost:8080/addemployee", requestBody)
+  //       .then((res) => console.log(res.data));
+  //   };
   return (
     <div>
       <h1>Axios Requests</h1>
-      <button onClick={() => getPosts()}>Get posts</button>
+      {/* <button onClick={() => getPosts()}>Get posts</button> */}
       <button onClick={() => post()}>Post</button>
-      <button onClick={() => getUser()}>get user</button>
-      <button onClick={() => postUser()}>post user</button>
+      {/* <button onClick={() => getUser()}>get user</button>
+      <button onClick={() => postUser()}>post user</button> */}
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              {post.id} {post.title}{" "}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
